0a83e5c682c3a621fe008e5acab8919c30b1ec75
JBrewer33 <brewerj@usc.edu>
2026-05-04 10:42:54 -0700

    Add: observer - submission-0

---
+++ Design Patterns/observer/submission-0.py
@@ -0,0 +1,33 @@
+class Observer(ABC):
+    @abstractmethod
+    def notify(self, itemName: str) -> None:
+        pass
+
+class Customer(Observer):
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.notifications = 0
+
+    def notify(self, itemName: str) -> None:
+        self.notifications += 1
+
+    def countNotifications(self) -> int:
+        return self.notifications
+
+class OnlineStoreItem:
+    def __init__(self, itemName: str, stock: int) -> None:
+        self.itemName = itemName
+        self.stock = stock
+        self.subscribed = set()
+
+    def subscribe(self, observer: Observer) -> None:
+        self.subscribed.add(observer)
+
+    def unsubscribe(self, observer: Observer) -> None:
+        self.subscribed.remove(observer)
+
+    def updateStock(self, newStock: int) -> None:
+        if self.stock == 0 and newStock > self.stock:
+            for subscriber in self.subscribed:
+                subscriber.notify(self.itemName)
+        self.stock = newStock
