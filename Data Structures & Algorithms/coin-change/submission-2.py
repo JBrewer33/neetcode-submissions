d1be545a61951d28fbb6b88c13f5c52a1d29b9ad
JBrewer33 <brewerj@usc.edu>
2026-05-06 17:47:02 -0700

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,20 @@
+#dp not greedy due to varying coin values
+#decision at every step, which coin to take
+#want to minimize so build array with minimum coins up to that point 1d
+#dp[i] = min coints up to point or numb
+#dp[i] = f(dp[i-1] + coin)
+#bc count > value
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        amountsDp = [float('inf')] * (amount + 1) #store min number of coints to make amount i
+        amountsDp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    amountsDp[i] = min(amountsDp[i - coin] + 1, amountsDp[i])
+        if amountsDp[amount] != float('inf'):
+            return amountsDp[amount]
+        return -1
